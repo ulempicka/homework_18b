8e298726e328c0d76d25281857fc35079880acde
ulempicka <urszula.lempicka@gmail.com>
2020-08-05 18:35:18 +0200

    Commit1 - updated 3 tests from part one

--- src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -1,19 +1,28 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FilteringMachine {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+        return numberList.stream()
+                .filter(number -> (number % 2 == 0))
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+        return numberList.stream()
+                .filter(number -> (number >= 20))
+                .collect(Collectors.toList());
     }
 
     public List<Book> convertToBooks(List<String> titles) {
-        return null;
+        return titles.stream()
+                .map(Book::new)
+                .collect(Collectors.toList());
     }
 
     // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
